1bdd4ec96535c2d5c92df168689f305d2d7bcec7
Hossein Poorhemati <hossein.poorhemati@gmail.com>
2025-03-06 20:14:42 +0000

    adding macros

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -9,3 +9,11 @@ SELECT *
  UNION ALL
  SELECT *
  FROM {{ref('stg_raw__facebook')}}
+
+
+ -- or if the dbt_util package was installed, only this: 
+--   {{
+--      dbt_utils.union_relations(
+--          relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
+--      )
+--  }}
