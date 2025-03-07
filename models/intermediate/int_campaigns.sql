49782887aa60208768d4c09bdd3dc4d3983b2f82
Hossein Poorhemati <hossein.poorhemati@gmail.com>
2025-03-07 02:17:51 +0000

    final

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,3 +1,5 @@
+
+
 SELECT *
  FROM {{ref('stg_raw__adwords')}}
  UNION ALL
@@ -6,14 +8,6 @@ SELECT *
  UNION ALL
  SELECT *
  FROM {{ref('stg_raw__criteo')}}
- UNION ALL
+ UNION ALL 
  SELECT *
  FROM {{ref('stg_raw__facebook')}}
-
-
- -- or if the dbt_util package was installed, only this: 
---   {{
---      dbt_utils.union_relations(
---          relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')]
---      )
---  }}
